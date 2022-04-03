518716d07c33719ccc6620322f3ed84c19b2cdeb
eren <erndemir.1@gmail.com>
2022-04-03 13:48:04 +0300

    redis added

--- consumer.js
+++ consumer.js
@@ -1,28 +1,40 @@
-const amqp = require('amqplib')
+const amqp = require("amqplib");
 const data = require("./data.json");
+const redis = require("redis");
+const client = redis.createClient({ legacyMode: true });
 
-const queueName = process.argv[2] || "jobsQueue"
+const queueName = process.argv[2] || "jobsQueue";
 console.log("qu", queueName);
 connectRabbitMq();
 
+(async () => {
+  await client.connect();
+})();
+
 async function connectRabbitMq() {
-    try {
-        const connection = await amqp.connect("amqp://127.0.0.1:5672");
-        const channel = await connection.createChannel();
-        const assertion = await channel.assertQueue(queueName);
-        console.log("Mesaj Bekleniyor...");
-        channel.consume(queueName, (message) => {
-            const messageInfo = JSON.parse(message.content.toString())
-            const userInfo = data.find(u => u.id == messageInfo.description)
-            if (userInfo) {
-                console.log("İşlenen kayıt", userInfo)
-                channel.ack(message)
+  try {
+    const connection = await amqp.connect("amqp://127.0.0.1:5672");
+    const channel = await connection.createChannel();
+    const assertion = await channel.assertQueue(queueName);
+    console.log("Mesaj Bekleniyor...");
+    channel.consume(queueName, (message) => {
+      const messageInfo = JSON.parse(message.content.toString());
+      const userInfo = data.find((u) => u.id == messageInfo.description);
+      if (userInfo) {
+        console.log("İşlenen kayıt", userInfo);
+        client.set(
+          `user_${userInfo.id}`,
+          JSON.stringify(userInfo),
+          (err, status) => {
+            if (!err) {
+              console.log("Status:", status);
+              channel.ack(message);
             }
-
-        })
-
-
-    } catch (e) {
-        console.log("Hata", e.message);
-    }
+          }
+        );
+      }
+    });
+  } catch (e) {
+    console.log("Hata", e.message);
+  }
 }
